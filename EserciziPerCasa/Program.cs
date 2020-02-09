bf4c5cc92fd5deacc8e37c0fa3b504727c5a82ad
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2020-02-09 18:01:49 +0100

    Add project files.

---
+++ EserciziPerCasa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EserciziPerCasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numeri = new List<double>();
+            int c = 0;
+            while(c<10)
+            {
+                Console.WriteLine("inserisci un numero:");
+                double a = double.Parse(Console.ReadLine());
+                c++;
+                if(numeri.Contains(a))
+                {
+                    Console.WriteLine($"il valore {a} è gia contenuto nella lista.");
+                }
+                numeri.Add(a);
+            }
+        }
+    }
+}
