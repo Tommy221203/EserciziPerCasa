f4cc65b110928fb991bf7a5efb51e77896db198f
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2020-02-09 18:22:48 +0100

    finiti 2 e terzo esercizio

--- EserciziPerCasa/Program.cs
+++ EserciziPerCasa/Program.cs
@@ -7,19 +7,47 @@ namespace EserciziPerCasa
     {
         static void Main(string[] args)
         {
-            List<double> numeri = new List<double>();
+            Console.WriteLine("primo esercizio");
+            List<double> numeri1 = new List<double>();
             int c = 0;
-            while(c<10)
+            while(c<1)
             {
                 Console.WriteLine("inserisci un numero:");
                 double a = double.Parse(Console.ReadLine());
                 c++;
-                if(numeri.Contains(a))
+                if(numeri1.Contains(a))
                 {
                     Console.WriteLine($"il valore {a} è gia contenuto nella lista");
                 }
-                numeri.Add(a);
+                numeri1.Add(a);
             }
+            Console.WriteLine("secondo esercizio");
+            List<double> numeri2 = new List<double>();
+            c = 0;
+            Random rnd = new Random();
+            while (c<1)
+            {
+                double b = rnd.Next(1, 15);
+                c++;
+                numeri2.Add(b);
+                numeri2.Sort();
+            }
+            Console.WriteLine("terzo esercizio");
+            List<double> numeri3 = new List<double>();
+            c = 0;
+            while(c<30)
+            {
+                double d = rnd.Next(1, 1000);
+                c++;
+                if (d % 2 == 0)
+                    numeri3.Add(d);numeri3.Sort();
+            }
+            for(int i=0;i<numeri3.Count;i++)
+            {
+                Console.WriteLine($"{numeri3[i]}");
+            }
+
+
         }
     }
 }
